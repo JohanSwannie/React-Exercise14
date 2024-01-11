7e83c9e6eeb0de0268a8e7a05491c04897d817c0
Johan Swan <jswannieman@gmail.com>
2024-01-12 09:38:11 +1300

    Update InputBox JSX Component

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -15,7 +15,10 @@ function InputBox({
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1-2">
-        <label htmlFor={id} className="text-black/100 mb-2 inline-block">
+        <label
+          htmlFor={id}
+          className="text-black/100 mb-2 font-bold first-line:inline-block"
+        >
           {label}
         </label>
         <input
@@ -31,7 +34,7 @@ function InputBox({
         />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
-        <p className="text-black/100 mb-2 w-full">Currency Type</p>
+        <p className="text-black/100 font-bold mb-2 w-full">Currency Type</p>
         <select
           className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
           value={selectedCurrency}

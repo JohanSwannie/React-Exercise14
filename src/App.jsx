6688fa4de748e6899f21962767689819df6b79cb
Johan Swan <jswannieman@gmail.com>
2024-01-12 09:34:22 +1300

    Update App JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,6 @@ function App() {
           >
             <div className="w-full mb-1">
               <InputBox
-                className="font-bold"
                 label="from"
                 amount={amount}
                 currencyOptions={options}

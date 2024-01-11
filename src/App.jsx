9ed7a5d5f92cd6bf7386e71c418a7cbe1200b5ce
Johan Swan <jswannieman@gmail.com>
2024-01-12 08:49:02 +1300

    Update App JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { useState } from "react";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1 className="bg-green-400 text-white">Vite + React</h1>
     </>
   );
 }
